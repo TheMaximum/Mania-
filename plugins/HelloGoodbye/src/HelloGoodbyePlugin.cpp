7d306502f559c488fb84e3bd8f5c36939dd3cbfe
TheMaximum <maxklaversma@gmail.com>
2016-12-29 23:04:01 +0100

    Add example of server query in plugin.

--- plugins/HelloGoodbye/src/HelloGoodbyePlugin.cpp
+++ plugins/HelloGoodbye/src/HelloGoodbyePlugin.cpp
@@ -14,6 +14,17 @@ void HelloGoodbyePlugin::Init()
 void HelloGoodbyePlugin::OnPlayerConnect(Player player)
 {
     std::cout << "PLUGIN Player Connected: " << player.Login << "!" << std::endl;
+
+    GbxParameters* params = new GbxParameters();
+    std::string login = player.Login;
+    params->Put(&login);
+
+    GbxMessage* message = new GbxMessage("GetDetailedPlayerInfo", params);
+    if(server->Query(message))
+    {
+        std::vector<GbxResponseParameter> responseParams = server->GetResponse()->GetParameters();
+        std::cout << "PLUGIN Response: " << responseParams.at(0).Type << std::endl;
+    }
 }
 
 void HelloGoodbyePlugin::OnPlayerDisconnect(Player player)
